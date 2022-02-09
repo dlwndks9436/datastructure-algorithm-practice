fc1f9c0b1f9bdb0be71afa60625032a22ea80eb9
dlwndks9436 <dlwndks9436@naver.com>
2022-02-09 12:20:49 +0900

    K-diff Pairs in an Array

---
+++ leetCode/daily-challenge/k-diff-pairs-in-an-array/app.ts
@@ -0,0 +1,55 @@
+function findPairs(nums: number[], k: number): number {
+  let count = 0;
+  const numMap = new Map<number, number>();
+  nums.forEach((num) => {
+    numMap.has(num)
+      ? numMap.set(num, (numMap.get(num) as number) + 1)
+      : numMap.set(num, 1);
+  });
+
+  while (nums.length > 0) {
+    const lastNum = nums[nums.length - 1];
+    const offset = lastNum - k;
+    if (numMap.has(offset)) {
+      if (k === 0 && (numMap.get(offset) as number) < 2) {
+      } else {
+        count++;
+        numMap.delete(offset);
+      }
+    }
+    nums.pop();
+  }
+
+  return count;
+}
+
+const nums = [1, 2, 4, 4, 3, 3, 0, 9, 2, 3];
+const k = 3;
+
+const output = findPairs(nums, k);
+console.log(output);
+
+// Given an array of integers nums and an integer k, return the number of unique k-diff pairs in the array.
+
+// A k-diff pair is an integer pair (nums[i], nums[j]), where the following are true:
+
+// 0 <= i < j < nums.length
+// |nums[i] - nums[j]| == k
+// Notice that |val| denotes the absolute value of val.
+
+// Example 1:
+
+// Input: nums = [3,1,4,1,5], k = 2
+// Output: 2
+// Explanation: There are two 2-diff pairs in the array, (1, 3) and (3, 5).
+// Although we have two 1s in the input, we should only return the number of unique pairs.
+// Example 2:
+
+// Input: nums = [1,2,3,4,5], k = 1
+// Output: 4
+// Explanation: There are four 1-diff pairs in the array, (1, 2), (2, 3), (3, 4) and (4, 5).
+// Example 3:
+
+// Input: nums = [1,3,1,5,4], k = 0
+// Output: 1
+// Explanation: There is one 0-diff pair in the array, (1, 1).
