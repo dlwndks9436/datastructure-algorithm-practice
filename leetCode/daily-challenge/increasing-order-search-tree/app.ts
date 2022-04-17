9f5be48672bffb747776fab78c8346e143d2cdb7
dlwndks9436 <dlwndks9436@naver.com>
2022-04-17 13:06:44 +0900

    Increasing Order Search Tree

---
+++ leetCode/daily-challenge/increasing-order-search-tree/app.ts
@@ -0,0 +1,42 @@
+// Definition for a binary tree node.
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function increasingBST(root: TreeNode | null): TreeNode | null {
+  const resultNode = new TreeNode(0);
+  let currentNode = resultNode;
+  arrangeNode(root);
+
+  return resultNode.right;
+  function arrangeNode(node: TreeNode | null) {
+    if (!node) return;
+    arrangeNode(node.left);
+    currentNode.right = new TreeNode(node.val);
+    currentNode = currentNode.right;
+    arrangeNode(node.right);
+  }
+}
+
+// Given the root of a binary search tree, rearrange the tree in in-order so that the leftmost node in the tree is now the root of the tree, and every node has no left child and only one right child.
+
+// Example 1:
+
+// Input: root = [5,3,6,2,4,null,8,1,null,null,null,7,9]
+// Output: [1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null,9]
+// Example 2:
+
+// Input: root = [5,1,7]
+// Output: [1,null,5,null,7]
+
+// Constraints:
+
+// The number of nodes in the given tree will be in the range [1, 100].
+// 0 <= Node.val <= 1000
