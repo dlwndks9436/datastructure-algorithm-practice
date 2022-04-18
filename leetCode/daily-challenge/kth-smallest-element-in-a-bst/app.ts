a8fb1c15c69d4333661e9c26e1dd8637b051a825
dlwndks9436 <dlwndks9436@naver.com>
2022-04-18 21:13:37 +0900

    Kth Smallest Element in a BST

---
+++ leetCode/daily-challenge/kth-smallest-element-in-a-bst/app.ts
@@ -0,0 +1,46 @@
+// Definition for a binary tree node.
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function kthSmallest(root: TreeNode | null, k: number): number {
+  let result = 0;
+  traverseNode(root);
+  return result;
+  function traverseNode(node: TreeNode | null) {
+    if (!node) return null;
+    traverseNode(node.left);
+    k--;
+    if (k === 0) {
+      result = node.val;
+      return null;
+    }
+    traverseNode(node.right);
+  }
+}
+
+// Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
+
+// Example 1:
+
+// Input: root = [3,1,4,null,2], k = 1
+// Output: 1
+// Example 2:
+
+// Input: root = [5,3,6,2,4,null,null,1], k = 3
+// Output: 3
+
+// Constraints:
+
+// The number of nodes in the tree is n.
+// 1 <= k <= n <= 104
+// 0 <= Node.val <= 104
+
+// Follow up: If the BST is modified often (i.e., we can do insert and delete operations) and you need to find the kth smallest frequently, how would you optimize?
